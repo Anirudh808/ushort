8399be6b0988371767f8fffd2ea9efdd49b8b4f9
Anirudh <anirudhmounasamy@gmail.com>
2025-08-04 20:58:13 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/shorturl/", // This must match your repository name exactly
+  base: "/", // This must match your repository name exactly
   build: {
     outDir: "dist",
   },
